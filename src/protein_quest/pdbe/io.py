b36eb95d0a3f5dc1078ec7e529e9e1913cf05451
Stefan Verhoeven <stefan.verhoeven@gmail.com>
2025-08-13 14:56:54 +0200

    do not filter on min/max residues aswell just write file with single chain or count residues

--- src/protein_quest/pdbe/io.py
+++ src/protein_quest/pdbe/io.py
@@ -1,5 +1,4 @@
 import logging
-from dataclasses import dataclass
 from pathlib import Path
 
 import gemmi
@@ -41,79 +40,89 @@ def first_chain_from_uniprot_chains(uniprot_chains: str) -> str:
     return chain
 
 
-def filter_and_write_single_chain_pdb_file(
-    mmcif_file: Path | str,
-    chain2keep: str,
-    output_file: Path | str,
+def is_chain_in_residues_range(
+    file: Path | str,
     min_residues: int,
     max_residues: int,
-    out_chain: str = "A",
-) -> tuple[bool, int]:
-    """Saves a specific protein chain from a mmCIF file to a new PDB file.
+    chain: str = "A",
+) -> bool:
+    """Checks if a specific chain in a mmCIF/pdb file has a number of residues within a specified range.
 
     Args:
-        mmcif_file: Path to the input mmCIF file.
-        chain2keep: Chain to keep.
-        output_file: Path to the output PDB file.
-        min_residues: Minimum number of residues in the chain to write.
-        max_residues: Maximum number of residues in the chain to write.
-        out_chain: Chain identifier for the saved chain in the output file.
+        file: Path to the input mmCIF/pdb file.
+        min_residues: Minimum number of residues.
+        max_residues: Maximum number of residues.
+        chain: Chain to check.
 
     Returns:
-        A tuple containing a boolean indicating whether
-        chain is in the residue range and the file was written successfully,
-        and the number of residues in the chain.
+        True if the chain has a number of residues within the specified range, False otherwise.
     """
-    structure = gemmi.read_structure(str(mmcif_file))
+    nr_residues = nr_residues_in_chain(file, chain)
+    return min_residues <= nr_residues <= max_residues
+
+
+def nr_residues_in_chain(file: Path | str, chain: str = "A") -> int:
+    """Returns the number of residues in a specific chain from a mmCIF/pdb file.
+
+    Args:
+        file: Path to the input mmCIF/pdb file.
+        chain: Chain to count residues of.
+
+    Returns:
+        The number of residues in the specified chain.
+    """
+    structure = gemmi.read_structure(str(file))
+    model = structure[0]
+    gchain = find_chain_in_model(model, chain)
+    if gchain is None:
+        return 0
+    return len(gchain)
+
+
+def find_chain_in_model(model: gemmi.Model, wanted_chain: str) -> gemmi.Chain | None:
+    chain = model.find_chain(wanted_chain)
+    if chain is None:
+        # For chain A in 4v92 the find_chain method returns None,
+        # however it is prefixed with 'B',
+        # so we try again as last char of chain name
+        mchains = [c for c in model if c.name.endswith(wanted_chain)]
+        if mchains:
+            return mchains[0]
+    return chain
+
+
+def write_single_chain_pdb_file(
+    input_file: Path, uniprot_chain: str, uniprot_acc: str, output_dir: Path, out_chain: str = "A"
+) -> Path | None:
+    """Write a single chain PDB file from a mmCIF file.
+
+    Args:
+        input_file: Path to the input mmCIF file.
+        uniprot_chain: The UniProt chain to keep.
+        uniprot_acc: The UniProt accession number.
+        output_dir: Directory to save the output PDB file.
+        out_chain: The chain identifier for the output PDB file.
+
+    Returns:
+        Path to the output PDB file or None if not created.
+    """
+    chain2keep = first_chain_from_uniprot_chains(uniprot_chain)
+
+    structure = gemmi.read_structure(str(input_file))
     model = structure[0]
 
     # Only count residues of polymer
     model.remove_ligands_and_waters()
 
-    chain = model.find_chain(chain2keep)
+    chain = find_chain_in_model(model, chain2keep)
     if chain is None:
-        # For chain A in 4v92 the find_chain method returns None,
-        # however it is prefixed with 'B',
-        # so we try again as last char of chain name
-        mchains = [c for c in model if c.name.endswith(chain2keep)]
-        if mchains:
-            chain = mchains[0]
-    if chain is None:
-        # TODO use _struct_ref_seq in cif file to find uniprot accession
         logger.warning(
             "Chain %s not found in %s. Skipping.",
             chain2keep,
-            mmcif_file,
+            input_file,
         )
-        return False, 0
-
-    nr_residues = len(chain)
-    if nr_residues < min_residues:
-        logger.info(
-            "Skipping %s, because it has too few residues in chain %s: %d < %d.",
-            mmcif_file,
-            chain2keep,
-            nr_residues,
-            min_residues,
-        )
-        return False, nr_residues
-    if nr_residues > max_residues:
-        logger.info(
-            "Skipping %s, because it has too many residues in chain %s: %d > %d.",
-            mmcif_file,
-            chain2keep,
-            nr_residues,
-            max_residues,
-        )
-        return False, nr_residues
-    logger.info(
-        'From %s taking chain "%s", with %d residues and saving as "%s" with chain %s.',
-        mmcif_file,
-        chain2keep,
-        nr_residues,
-        output_file,
-        out_chain,
-    )
+        return None
+    output_file = output_dir / f"{uniprot_acc}_{input_file.stem}_{chain}2A.pdb"
 
     new_structure = gemmi.Structure()
     new_structure.resolution = structure.resolution
@@ -134,134 +143,4 @@ def filter_and_write_single_chain_pdb_file(
     new_structure.add_model(new_model)
     new_structure.write_pdb(str(output_file))
 
-    # TODO use less diskspace, save gzipped and make powerfit work with it
-    return True, nr_residues
-
-
-@dataclass(frozen=True)
-class ProteinPdbRow:
-    """Info about PDB entry and its relation to an Uniprot entry
-
-    Parameters:
-        id: The PDB ID of the entry.
-        uniprot_chains: The UniProt chains associated with the PDB entry.
-        uniprot_acc: The UniProt accession number associated with the PDB entry.
-        mmcif_file: The path to the mmCIF file for the PDB entry, or None if not retrieved yet.
-    """
-
-    id: str
-    uniprot_chains: str
-    uniprot_acc: str
-    mmcif_file: Path | None
-
-
-@dataclass(frozen=True)
-class SingleChainQuery:
-    """Query for writing single chain PDB files.
-
-    Parameters:
-        min_residues: Minimum number of residues that must be in chain.
-        max_residues: Maximum number of residues that must be in chain.
-    """
-
-    min_residues: int
-    max_residues: int
-
-
-@dataclass(frozen=True)
-class SingleChainResult:
-    """Result of writing a single chain PDB file.
-
-    Parameters:
-        uniprot_acc: The UniProt accession.
-        pdb_id: The PDB ID of the entry.
-        output_file: The path to the output PDB file with
-            just the first chain (renamed to A) belonging to given Uniprot accession.
-            Only set when passed is True.
-        nr_residues: The number of residues in the chain that was written.
-        passed: Whether the chain passed the number of residue filter.
-    """
-
-    uniprot_acc: str
-    pdb_id: str
-    output_file: Path | None
-    nr_residues: int
-    passed: bool
-
-
-def nr_residues_in_chain(file: Path | str, chain: str = "A") -> int:
-    """Returns the number of residues in a specific chain from a mmCIF/pdb file.
-
-    Args:
-        file: Path to the input mmCIF/pdb file.
-        chain: Chain to keep.
-
-    Returns:
-        The number of residues in the specified chain.
-    """
-    structure = gemmi.read_structure(str(file))
-    model = structure[0]
-    return len(model[chain])
-
-
-def write_single_chain_pdb_file(
-    proteinpdb: ProteinPdbRow,
-    session_dir: Path,
-    single_chain_dir: Path,
-    query: SingleChainQuery,
-) -> SingleChainResult:
-    """Process a ProteinPdbRow to write a single chain PDB file if possible, returning the result.
-
-    Args:
-        proteinpdb: A ProteinPdbRow object.
-        session_dir: The directory where the session files are stored.
-        single_chain_dir: The directory where the single chain PDB files will be saved.
-        query: The query containing the minimum and maximum number of residues.
-
-    Returns:
-        Result object containing the output file path and whether the chain passed the residue filter.
-    """
-    if not proteinpdb.mmcif_file:
-        logger.warning(
-            "Skipping %s, because it does not have a file.",
-            proteinpdb.id,
-        )
-        return SingleChainResult(
-            uniprot_acc=proteinpdb.uniprot_acc,
-            pdb_id=proteinpdb.id,
-            output_file=None,
-            nr_residues=0,
-            passed=False,
-        )
-
-    mmcif_file = proteinpdb.mmcif_file
-    chain2keep = first_chain_from_uniprot_chains(proteinpdb.uniprot_chains)
-    uniprot_acc = proteinpdb.uniprot_acc
-    mmmcif_filename = mmcif_file.name.replace(".cif", "").replace(".gz", "")
-    output_file = single_chain_dir / f"{uniprot_acc}_{mmmcif_filename}_{chain2keep}2A.pdb"
-
-    if output_file.exists():
-        logger.info(
-            f"Output file {output_file} already exists. Skipping saving single chain PDB file for {mmcif_file}.",
-        )
-        nr_residues = nr_residues_in_chain(output_file)
-        passed = True
-    else:
-        passed, nr_residues = filter_and_write_single_chain_pdb_file(
-            mmcif_file=mmcif_file,
-            chain2keep=chain2keep,
-            output_file=output_file,
-            min_residues=query.min_residues,
-            max_residues=query.max_residues,
-        )
-
-    real_output_file = None
-    if passed:
-        real_output_file = output_file.relative_to(session_dir)
-    return SingleChainResult(
-        uniprot_acc=uniprot_acc,
-        pdb_id=proteinpdb.id,
-        output_file=real_output_file,
-        nr_residues=nr_residues,
-        passed=passed,
-    )
+    return output_file
