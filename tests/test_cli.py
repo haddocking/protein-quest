7be3a3d538eb5ca1438a18a0c8545ad37ec0ad75
Stefan Verhoeven <stefan.verhoeven@gmail.com>
2025-11-17 13:25:11 +0100

    More cli tests

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,3 +1,4 @@
+import csv
 from pathlib import Path
 from textwrap import dedent
 
@@ -99,3 +100,206 @@ def test_search_uniprot_details(tmp_path: Path, capsys: pytest.CaptureFixture[st
     captured = capsys.readouterr()
     assert "Retrieving UniProt entry details for 2 uniprot accessions" in captured.err
     assert "Retrieved details for 2 UniProt entries, written to " in captured.err
+
+
+@pytest.mark.vcr
+def test_search_alphafold(tmp_path: Path, capsys: pytest.CaptureFixture[str]):
+    input_text = tmp_path / "uniprot_accessions.txt"
+    input_text.write_text("P00811\n")
+    output_file = tmp_path / "af_results.csv"
+
+    argv = [
+        "search",
+        "alphafold",
+        str(input_text),
+        str(output_file),
+    ]
+
+    main(argv)
+
+    result = output_file.read_text()
+
+    expected = dedent("""\
+        uniprot_accession,af_id
+        P00811,P00811
+        """)
+    assert result == expected
+
+    captured = capsys.readouterr()
+    assert "Finding AlphaFold entries for 1 uniprot accessions" in captured.err
+    assert "Found 1 AlphaFold entries, written to " in captured.err
+
+
+def test_filter_chain_happy_path(sample2_cif: Path, tmp_path: Path, capsys: pytest.CaptureFixture[str]):
+    chains_fn = tmp_path / "chains.csv"
+    chains_fn.write_text("pdb_id,chain\n2Y29,A\n")
+
+    argv = [
+        "filter",
+        "chain",
+        str(chains_fn),
+        str(sample2_cif.parent),
+        str(tmp_path),
+    ]
+
+    main(argv)
+
+    output_file = tmp_path / "2Y29_A2A.cif.gz"
+    assert output_file.exists()
+
+    captured = capsys.readouterr()
+    assert "Wrote 1 single-chain PDB/mmCIF files to" in captured.err
+
+
+def test_filter_chain_input_file_notfound(tmp_path: Path, capsys: pytest.CaptureFixture[str]):
+    input_dir = tmp_path / "input"
+    input_dir.mkdir()
+    output_dir = tmp_path / "output"
+    output_dir.mkdir()
+    chains_fn = tmp_path / "chains.csv"
+    chains_fn.write_text("pdb_id,chain\n2Y29,A\n")
+
+    argv = [
+        "filter",
+        "chain",
+        str(chains_fn),
+        str(input_dir),
+        str(output_dir),
+    ]
+
+    with pytest.raises(SystemExit):
+        main(argv)
+
+    assert not any(output_dir.iterdir())
+
+    captured = capsys.readouterr()
+    assert "No structure file found for 2Y29" in captured.err
+
+
+def test_filter_residue(sample_cif: Path, sample2_cif: Path, tmp_path: Path, capsys: pytest.CaptureFixture[str]):
+    input_dir = tmp_path / "input"
+    input_dir.mkdir()
+    local_sample = input_dir / sample_cif.name
+    local_sample.symlink_to(sample_cif)
+    local_sample2 = input_dir / sample2_cif.name
+    local_sample2.symlink_to(sample2_cif)
+    output_dir = tmp_path / "output"
+    output_dir.mkdir()
+    stats_fn = tmp_path / "stats.csv"
+
+    argv = [
+        "filter",
+        "residue",
+        str(input_dir),
+        str(output_dir),
+        "--min-residues",
+        "100",
+        "--max-residues",
+        "200",
+        "--copy-method",
+        "symlink",
+        "--write-stats",
+        str(stats_fn),
+    ]
+
+    main(argv)
+
+    # Check output files
+    output_files = list(output_dir.iterdir())
+    assert len(output_files) == 1
+    expected_passed_file = output_dir / sample_cif.name
+    assert expected_passed_file in output_files
+
+    # Check stats file
+    with stats_fn.open() as f:
+        rows = list(csv.DictReader(f))
+    # Input files processed in alphabetical order
+    expected_stats = [
+        {
+            "input_file": str(local_sample2),
+            "residue_count": "8",
+            "passed": "False",
+            "output_file": "",
+        },
+        {
+            "input_file": str(local_sample),
+            "residue_count": "173",
+            "passed": "True",
+            "output_file": str(expected_passed_file),
+        },
+    ]
+    assert rows == expected_stats
+
+    # Check captured output
+    captured = capsys.readouterr()
+    assert "by number of residues in chain A" in captured.err
+    assert "Wrote 1 files to" in captured.err
+    assert "Statistics written to" in captured.err
+
+
+def test_filter_secondary_structure(
+    sample_cif: Path, sample2_cif: Path, tmp_path: Path, capsys: pytest.CaptureFixture[str]
+):
+    input_dir = tmp_path / "input"
+    input_dir.mkdir()
+    local_sample = input_dir / sample_cif.name
+    local_sample.symlink_to(sample_cif)
+    local_sample2 = input_dir / sample2_cif.name
+    local_sample2.symlink_to(sample2_cif)
+    output_dir = tmp_path / "output"
+    output_dir.mkdir()
+    stats_fn = tmp_path / "ss_stats.csv"
+
+    argv = [
+        "filter",
+        "secondary-structure",
+        str(input_dir),
+        str(output_dir),
+        "--abs-min-helix-residues",
+        "10",
+        "--copy-method",
+        "symlink",
+        "--write-stats",
+        str(stats_fn),
+    ]
+
+    main(argv)
+
+    # Check output files
+    output_files = list(output_dir.iterdir())
+    assert len(output_files) == 1
+    expected_passed_file = output_dir / sample_cif.name
+    assert expected_passed_file in output_files
+
+    # Check stats file
+    with stats_fn.open() as f:
+        rows = list(csv.DictReader(f))
+    expected_stats = [
+        {
+            "helix_ratio": "0.0",
+            "input_file": str(local_sample2),
+            "nr_helix_residues": "0",
+            "nr_residues": "8",
+            "nr_sheet_residues": "0",
+            "output_file": "",
+            "passed": "False",
+            "sheet_ratio": "0.0",
+        },
+        {
+            "input_file": str(local_sample),
+            "nr_residues": "173",
+            "nr_helix_residues": "58",
+            "nr_sheet_residues": "59",
+            "helix_ratio": f"{58 / 173:.3f}",
+            "sheet_ratio": f"{59 / 173:.3f}",
+            "passed": "True",
+            "output_file": str(expected_passed_file),
+        },
+    ]
+    assert rows == expected_stats
+
+    # Check captured output
+    captured = capsys.readouterr()
+    assert "by secondary structure" in captured.err
+    assert "Wrote 1 files to" in captured.err
+    assert "Statistics written to" in captured.err
