56b64f7b522d7697746b74db7e2a9705fb2226ca
Stefan Verhoeven <stefan.verhoeven@gmail.com>
2025-11-17 12:39:27 +0100

    Add test for filter_files_on_residues()

--- tests/test_filters.py
+++ tests/test_filters.py
@@ -2,7 +2,12 @@ from pathlib import Path
 
 import pytest
 
-from protein_quest.filters import ChainFilterStatistics, filter_files_on_chain
+from protein_quest.filters import (
+    ChainFilterStatistics,
+    ResidueFilterStatistics,
+    filter_files_on_chain,
+    filter_files_on_residues,
+)
 from protein_quest.structure import ChainNotFoundError
 
 
@@ -33,6 +38,7 @@ def test_filter_files_on_chain_local_cluster(
         passed=True,
         output_file=tmp_path / "2Y29_A2A.cif.gz",
     )
+    assert expected_passed.output_file and expected_passed.output_file.exists()
     expected_discarded = ChainFilterStatistics(
         input_file=sample2_cif,
         chain_id="B",
@@ -44,3 +50,29 @@ def test_filter_files_on_chain_local_cluster(
 
     _, stderr = capsys.readouterr()
     assert expected_progress_bar in stderr
+
+
+def test_filter_files_on_residues(sample_cif: Path, sample2_cif: Path, tmp_path: Path):
+    results = list(
+        filter_files_on_residues(
+            input_files=[sample_cif, sample2_cif],
+            output_dir=tmp_path,
+            min_residues=100,
+            max_residues=200,
+        )
+    )
+    expected_passed = ResidueFilterStatistics(
+        input_file=sample_cif,
+        residue_count=173,
+        passed=True,
+        output_file=tmp_path / sample_cif.name,
+    )
+    assert expected_passed.output_file and expected_passed.output_file.exists()
+    expected_discarded = ResidueFilterStatistics(
+        input_file=sample2_cif,
+        residue_count=8,
+        passed=False,
+        output_file=None,
+    )
+
+    assert results == [expected_passed, expected_discarded]
